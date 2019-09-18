0e38ac37d6b459d852b2330a20b4194be029ec4f
DmytroKashchenko <kashchenkodi@gmail.com>
2019-09-18 19:45:08 +0300

    some changes

---
+++ src/main/java/ua/dmytrokashchenko/l4/Test.java
@@ -0,0 +1,10 @@
+package ua.dmytrokashchenko.l4;
+
+public class Test {
+    public static void main(String[] args) {
+        int f;
+        f = 5;
+        f = ++f + f++;
+        System.out.println(f);
+    }
+}
